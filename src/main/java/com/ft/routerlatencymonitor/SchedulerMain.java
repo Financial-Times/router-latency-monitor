aaace407e3f7a81d290c99f5f0f44fe6e98a74f9
Anurag Kapur <anurag@beancrunch.com>
2016-09-19 14:10:39 +0100

    Initialise with boilerplate scheduler project setup

---
+++ src/main/java/com/ft/routerlatencymonitor/SchedulerMain.java
@@ -0,0 +1,42 @@
+package com.ft.routerlatencymonitor;
+
+import org.quartz.Job;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.repeatSecondlyForever;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class SchedulerMain {
+
+    final static Logger logger = LoggerFactory.getLogger(SchedulerMain.class);
+
+    public static void main(String[] args) throws Exception {
+
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        JobDetail jobDetail = newJob(HelloJob.class).build();
+
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(repeatSecondlyForever(2))
+                .build();
+
+        scheduler.scheduleJob(jobDetail, trigger);
+    }
+
+    public static class HelloJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+            logger.info("HelloJob executed");
+        }
+    }
+}
